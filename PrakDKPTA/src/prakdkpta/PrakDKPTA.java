5f65af7d0e9e54c93879b733a66576f649a245d4
Asih001 <121451290+Asih001@users.noreply.github.com>
2023-06-01 12:25:40 +0700

    Add files via upload

---
+++ PrakDKPTA/src/prakdkpta/PrakDKPTA.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package prakdkpta;
+
+/**
+ *
+ * @author asihd
+ */
+public class PrakDKPTA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Login rg = new Login ();
+        rg.setVisible(true);
+        rg.pack();
+        rg.setLocationRelativeTo(null);
+        rg.setDefaultCloseOperation(Login.EXIT_ON_CLOSE);
+    }
+    
+}
